63778968b6cb038aded9cf7a955a29a77adea435
parthasarathiE <Pardhu448@users.noreply.github.com>
2020-01-26 12:15:17 +0530

    Update clix_config.py

--- dags/config/clix_config.py
+++ dags/config/clix_config.py
@@ -8,9 +8,9 @@ local_dst = '/usr/local/airflow/school_syncthing_data_live/'
 local_dst_static_vis = '/usr/local/airflow/syncthing_data_static_vis/'
 local_dst_state_data_logs = '/usr/local/airflow/state_level_data_logs/'
 
-remote_user = '<Host username>'
-remote_ip = '<server IP>'
-remote_passwd = '<server password>'
+remote_user = '<Host Username>'
+remote_ip = '<Host Server IP>'
+remote_passwd = '<Host server password>'
 states = ['mz', 'cg', 'ts', 'rj']
 #states = ['ts']
 static_visuals_states = ['mz', 'ts', 'cg', 'rj']
@@ -18,11 +18,11 @@ num_school_chunks = 4
 
 DB_TYPE = 'postgresql'
 DB_DRIVER = 'psycopg2'
-DB_USER = '<db admin username>'
-DB_PASS = '<db admin password>'
+DB_USER = '<backend db admin username>'
+DB_PASS = '<backend db admin password>'
 DB_HOST = '172.17.0.1'
-DB_PORT = '5433'
-DB_NAME = 'clix_dashboard_db'
+DB_PORT = '<backend db port>'
+DB_NAME = '<backend db name>'
 POOL_SIZE = 50
 SQLALCHEMY_DATABASE_URI = '%s+%s://%s:%s@%s:%s/%s' % (DB_TYPE, DB_DRIVER, DB_USER,
                                                   DB_PASS, DB_HOST, DB_PORT, DB_NAME)
